0a73c2267813e89c7240273f0b61e32f2e7a5e53
juncheolkim <kimjoon889@gmail.com>
2023-06-17 15:38:09 +0900

    feat : 페이지 나누기 2
    - 폴더 구조 나누기
    - 라우트 끼리, 컴포넌트 끼리 등
    
    1. 페이지 이동 도와주는 useNavigate()
    - 훅 : 유용한 것들이 들어있는 함수
    - 왜 씀! 이쁜 HTML 태그에 onClick 함수로 페이지 이동을 할 수 있다.
    - 그 왜에도 앞으로 가기(1), 뒤로가기(-1) 등등 기능이 있다.
    
    2. 404 페이지 만들기
    - 오타로 인해 잘못 접속했을 때 사용되어야한다.
    - path='*'
    - 설정된 라우트 이외 모든 path
    
    3. Nested Routes
    - nested toute 접속시엔 element 2개 보임
    - 내부 어디에 보여줄 지 정해야한다.
    - 상위 라우트에 <Outlet/>

--- src/App.js
+++ src/App.js
@@ -4,26 +4,43 @@ import { Navbar, Container, Nav, Row, Col } from "react-bootstrap";
 import bg from "./img/bg.png";
 import { useState } from "react";
 import data from "./data.js";
-import { Routes, Route, Link } from "react-router-dom";
-import Detail from "./detail";
+import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
+import Detail from "./routes/Detail";
 
 function App() {
     let [shoes] = useState(data);
+    let navigate = useNavigate();
 
     return (
         <div className="App">
             <Navbar bg="light" data-bs-theme="light">
                 <Container>
-                    <Navbar.Brand href="#home">Let's shop time</Navbar.Brand>
+                    <Navbar.Brand
+                        onClick={() => {
+                            navigate("/");
+                        }}
+                    >
+                        Let's shop time
+                    </Navbar.Brand>
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#features">Store</Nav.Link>
-                        <Nav.Link href="#pricing">Cart</Nav.Link>
+                        <Nav.Link
+                            onClick={() => {
+                                navigate("/");
+                            }}
+                        >
+                            Home
+                        </Nav.Link>
+                        <Nav.Link
+                            onClick={() => {
+                                navigate("/detail");
+                            }}
+                        >
+                            Detail
+                        </Nav.Link>
                     </Nav>
                 </Container>
             </Navbar>
-            <Link to="/">홈</Link>
-            <Link to="/detail">상세페이지</Link>
+
             <Routes>
                 <Route
                     path="/"
@@ -50,7 +67,24 @@ function App() {
                     }
                 />
                 <Route path="/detail" element={<Detail />} />
+                <Route path="/about" element={<About />}>
+                    <Route path="member" element={<div>멤버</div>}></Route>
+                    <Route
+                        path="location"
+                        element={<div>위치정보</div>}
+                    ></Route>
+                </Route>
+                <Route path="/*" element={<div>잘못된 URL 입니다.</div>} />
             </Routes>
+        </div>
+    );
+}
+
+function About() {
+    return (
+        <div>
+            <h4>회사정보</h4>
+            <Outlet />
         </div>
     );
 }
