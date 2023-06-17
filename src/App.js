e527863d7da8fbdf41c7bc6db8542ce47c83407e
juncheolkim <kimjoon889@gmail.com>
2023-06-17 14:21:58 +0900

    feat : 데이터 관리
    data.js에서 관리
    export/import 문법 사용.
    
    현재 데이터는 object자료형을 요소로 하는 array 자료형이다.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+/* eslint-disable */
 import "./App.css";
 import { Navbar, Container, Nav, Row, Col } from "react-bootstrap";
 import bg from "./img/bg.png";
+import { useState } from "react";
+import data from "./data.js";
 
 function App() {
+    let [shoes] = useState(data);
+
     return (
         <div className="App">
             <Navbar bg="light" data-bs-theme="light">
@@ -22,33 +27,25 @@ function App() {
             ></div>
             <Container>
                 <Row>
-                    <Col>
-                        <img
-                            src={process.env.PUBLIC_URL + "/img/shoes1.jpg"}
-                            width="80%"
-                        />
-                        <h4>상품명</h4>
-                        <p>상품설명</p>
-                    </Col>
-                    <Col>
-                        <img
-                            src={process.env.PUBLIC_URL + "/img/shoes2.jpg"}
-                            width="80%"
-                        />
-                        <h4>상품명</h4>
-                        <p>상품설명</p>
-                    </Col>
-                    <Col>
-                        <img
-                            src={process.env.PUBLIC_URL + "/img/shoes3.jpg"}
-                            width="80%"
-                        />
-                        <h4>상품명</h4>
-                        <p>상품설명</p>
-                    </Col>
+                    {shoes.map((item, idx) => {
+                        return <Shoes idx={idx} item={item} key={idx} />;
+                    })}
                 </Row>
             </Container>
         </div>
+    );
+}
+
+function Shoes(props) {
+    return (
+        <Col key={props.idx}>
+            <img
+                src={process.env.PUBLIC_URL + `/img/shoes${props.idx + 1}.jpg`}
+                width="180px"
+            />
+            <h4>{props.item.title}</h4>
+            <p>{props.item.content}</p>
+        </Col>
     );
 }
 
